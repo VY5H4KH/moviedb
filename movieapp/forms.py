a585233b8c1eb859bca222c0843926d9c32c6184
VY5H4KH <vyshakhmadhu3@gmail.com>
2023-09-15 14:40:27 +0530

    com

--- movieapp/forms.py
+++ movieapp/forms.py
@@ -5,3 +5,6 @@ class Movieform(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['Name','Mail_id','Your_Review']
+        widgets = {
+            'Your_Review': forms.Textarea(attrs={'rows': 3}),
+        }
